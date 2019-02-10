222cb0358244adc2af7caa26fe7f7d2735fb99ec
Ian Gomez <ian_gomez_50@yahoo.com.ph>
2019-02-09 22:21:03 -0600

    Quiz game Assignment 2

---
+++ question project/Assets/Question.cs
@@ -0,0 +1,21 @@
+﻿[System.Serializable]
+public class Question
+{
+    public string fact;
+    //public bool isTrue;
+
+    //public string opinion1;
+    public bool answer1;
+
+    //public string opinion2;
+    public bool answer2;
+
+    //public string opinion3;
+    public bool answer3;
+
+    //public string opinion4;
+    public bool answer4;
+
+
+}
+
