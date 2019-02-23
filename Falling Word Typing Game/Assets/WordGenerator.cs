bae0d10f0070138b8c5c501e631de18dbe0b4501
Ian Gomez <ian_gomez_50@yahoo.com.ph>
2019-02-23 08:59:15 -0600

    Assignment 3 Falling Word Game

---
+++ Falling Word Typing Game/Assets/WordGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+
+ 
+   // this grabs the words from the list of words, then it splits to words
+    private static TextAsset Listofwords = Resources.Load<TextAsset>("Listofwords");
+
+    public static string[] data = Listofwords.text.Split(new char[] { ',' });
+    
+    // FOR TESTING PURPOSES ONLY
+    //private static string[] wordList = { "test", "testing", "testoroni" };
+
+    public static string GetRandomWord()
+     {
+        int randomIndex = Random.Range(0, data.Length);
+        string randomWord = data[randomIndex];
+
+        return randomWord;
+     }
+    
+}
